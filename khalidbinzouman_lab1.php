4d9c8ce88753c2a114cbf2c137e5123fe77a5269
kb-wz <124049949+kb-wz@users.noreply.github.com>
2023-02-09 00:05:54 +0300

    Add files via upload

---
+++ khalidbinzouman_lab1.php
@@ -0,0 +1,31 @@
+
+
+ <h1> Khalid Binzouman 201911174 </h1>
+
+    <?php
+
+    $n = 1;
+    $s = "1";
+    $n1 = $n + $s;
+    $n2 = $n1 . "10 little penguins";
+    $n3 = "hello";
+    $n4 = "world";
+    $n5 = $n3 . $n4;
+    $n6 = $n3.$n4;
+   
+    echo "$n";
+    echo "<br>";
+    echo $s;
+    echo "<br>";
+    echo $n1;
+    echo "<br>";
+    echo $n2;
+    echo "<br>";
+    echo $n3;
+    echo "<br>";
+    echo $n4;
+    echo "<br>";
+    echo $n5;
+    echo "<br>";
+    echo $n6;
+    ?>
